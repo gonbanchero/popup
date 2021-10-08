4edf10a75ca4ec905d8b4b62dbc9488b78d5becb
Gon Banchero <gonbanchero@Imac.local>
2021-10-08 18:43:49 -0300

    botones

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let boton = document.getElementById("boton");
 let popup = document.getElementById("popup");
 let cerrar = document.getElementById("cerrar");
+let btn_number = document.getElementById("botones");
 
 boton.addEventListener("click", () => {
   popup.classList.remove("hidden");
@@ -9,3 +10,13 @@ boton.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
   popup.classList.add("hidden");
 });
+
+document.getElementById("1").onclick = GFG_click;
+document.getElementById("2").onclick = GFG_click;
+document.getElementById("3").onclick = GFG_click;
+document.getElementById("4").onclick = GFG_click;
+document.getElementById("5").onclick = GFG_click;
+
+function GFG_click() {
+  this.classList.add("active");
+}

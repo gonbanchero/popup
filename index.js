c0bb8f3722d54420c727631c302c92f63997e6fe
Gon Banchero <gonbanchero@Imac.local>
2021-10-14 15:26:27 -0300

    nuevas cositas js

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
-let boton = document.getElementById("boton");
-let popup = document.getElementById("popup");
-let cerrar = document.getElementById("cerrar");
-let btn_number = document.getElementById("botones");
+const openBtn = document.querySelector(".califica_btn");
+const closeBtn = document.querySelector(".svg");
+const contenedor = document.querySelector(".popup_container");
+const popup_items = document.querySelector(".popup-items");
+const calificationBtn = document.querySelectorAll(".boton");
+const botoneraBtn = document.querySelector(".botonera");
+const siguiente = document.querySelector(".siguiente");
+const mensaje = document.querySelector(".popup_message");
+const cerrarSvg = document.querySelector(".svg");
+const cerrarSvg2 = document.querySelector(".svg-2");
+let botones;
 
-boton.addEventListener("click", () => {
-  popup.classList.remove("hidden");
-});
+const openPopup = function () {
+  contenedor.classList.remove("hidden");
+};
 
-cerrar.addEventListener("click", () => {
-  popup.classList.add("hidden");
-});
+const cerrarPopup = function () {
+  contenedor.classList.add("hidden");
+};
 
-document.getElementById("1").onclick = GFG_click;
-document.getElementById("2").onclick = GFG_click;
-document.getElementById("3").onclick = GFG_click;
-document.getElementById("4").onclick = GFG_click;
-document.getElementById("5").onclick = GFG_click;
+const siguienteBtn = function () {
+  popup_items.classList.add("hidden");
+  mensaje.classList.remove("hidden");
+};
 
-function GFG_click() {
-  this.classList.add("active");
+openBtn.addEventListener("click", openPopup);
+
+for (let i = 0; i < calificationBtn.length; i++) {
+  calificationBtn[i].addEventListener("click", function () {
+    botones = calificationBtn[i];
+    botones.classList.add("active");
+  });
 }
+
+function desactivar() {
+  for (let i = 0; i < calificationBtn.length; i++) {
+    calificationBtn[i].classList.remove("active");
+  }
+}
+
+// const active = function () {
+//   for (let i = 0; i < calificationBtn.length; i++) {
+//     calificationBtn[i].classList.add("active");
+//   }
+// };
+
+// botoneraBtn.addEventListener("click", active);
+
+siguiente.addEventListener("click", () => {
+  if (
+    !document.querySelector(".checkbox").checked &&
+    botones.classList.contains("active")
+  ) {
+    alert("tienes que aceptar las condiciones");
+  } else if (
+    document.querySelector(".checkbox").checked &&
+    !botones.classList.contains("active")
+  ) {
+    alert("tienes que seleccionar una calificación");
+  } else {
+    siguienteBtn();
+  }
+});
+
+cerrarSvg.addEventListener("click", () => {
+  cerrarPopup();
+});
+
+cerrarSvg2.addEventListener("click", () => {
+  cerrarPopup();
+  popup_items.classList.remove("hidden");
+  mensaje.classList.add("hidden");
+  document.querySelector(".checkbox").checked = false;
+  desactivar();
+});

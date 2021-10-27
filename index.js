ea21383c5f96d03cb9ca0d03db0f5ccc4c7db1f4
gonbanchero <gonzabanchero@gmail.com>
2021-10-27 17:45:40 -0300

    final

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const cerrarSvg = document.querySelector(".svg");
 const cerrarSvg2 = document.querySelector(".svg-2");
 let botones;
 
+let btnSelected = true;
+
 const openPopup = function () {
   contenedor.classList.remove("hidden");
 };
@@ -23,40 +25,52 @@ const siguienteBtn = function () {
   mensaje.classList.remove("hidden");
 };
 
-openBtn.addEventListener("click", openPopup);
-
-for (let i = 0; i < calificationBtn.length; i++) {
-  calificationBtn[i].addEventListener("click", function () {
-    botones = calificationBtn[i];
-    botones.classList.add("active");
-  });
-}
-
 function desactivar() {
   for (let i = 0; i < calificationBtn.length; i++) {
     calificationBtn[i].classList.remove("active");
   }
 }
 
-// const active = function () {
-//   for (let i = 0; i < calificationBtn.length; i++) {
-//     calificationBtn[i].classList.add("active");
-//   }
-// };
+openBtn.addEventListener("click", openPopup);
 
-// botoneraBtn.addEventListener("click", active);
+function activateBtn() {
+  const btnList = [...calificationBtn]; // convierto nodelist en un array
+  btnList.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (
+        btnList.some((bt) => bt.classList.contains("active")) && // Si la lista contiene "some" que cumpla la condición
+        !btn.classList.contains("active") // && el botón actual no contiene la clase active
+      ) {
+        activeBtn = btnList.find((bt) => bt.classList.contains("active")); //Creo una variable y uso find para agarrar el botón activo.
+        activeBtn.classList.remove("active"); // Le saco la clase active a ese botón
+        btn.classList.add("active"); // Le agrego al botón apretado la clase active
+      } else {
+        // Si no pasa que hay un botón activo ni que el btn tocado este activo, simplemente agrega la clase al botón apretado
+        btn.classList.add("active");
+      }
+    });
+  });
+}
+
+activateBtn(); // Llamo a la función
 
 siguiente.addEventListener("click", () => {
+  const btnList = [...calificationBtn]; // convierto nodelist en un array
   if (
     !document.querySelector(".checkbox").checked &&
-    botones.classList.contains("active")
+    btnList.some((bt) => bt.classList.contains("active"))
   ) {
     alert("tienes que aceptar las condiciones");
   } else if (
     document.querySelector(".checkbox").checked &&
-    !botones.classList.contains("active")
+    !btnList.some((bt) => bt.classList.contains("active"))
   ) {
     alert("tienes que seleccionar una calificación");
+  } else if (
+    !document.querySelector(".checkbox").checked &&
+    !btnList.some((bt) => bt.classList.contains("active"))
+  ) {
+    alert("tienes que seleccionar una calificación y aceptar las condiciones");
   } else {
     siguienteBtn();
   }
